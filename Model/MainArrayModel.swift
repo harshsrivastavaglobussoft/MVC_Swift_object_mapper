949b09ea9b25b5dddb4841e8fb5cbbbba6a0ba95
harshsrivastava_globussoft <harshsrivastava@globussoft.in>
2017-08-21 15:23:23 +0530

    object mapper sample project

---
+++ Model/MainArrayModel.swift
@@ -0,0 +1,13 @@
+//
+//  MainArrayModel.swift
+//  DribbleInSwift
+//
+//  Created by Sumit Ghosh on 21/08/17.
+//  Copyright © 2017 Sumit Ghosh. All rights reserved.
+//
+
+import UIKit
+
+class MainArrayModel: NSObject {
+
+}
---
+++ Utility.swift
@@ -0,0 +1,13 @@
+//
+//  Utility.swift
+//  DribbleInSwift
+//
+//  Created by Sumit Ghosh on 21/08/17.
+//  Copyright © 2017 Sumit Ghosh. All rights reserved.
+//
+
+import UIKit
+
+class Utility: NSObject {
+
+}
